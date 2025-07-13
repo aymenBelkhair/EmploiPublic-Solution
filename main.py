d103dfa85ea55054e61871080d9ec79157a23a0e
aymenBelkhair <contact.aymenbelkhair@gmail.com>
2025-07-13 23:56:06 +0100

    quick update on the data scraped

--- main.py
+++ main.py
@@ -9,16 +9,26 @@ resp = s.get(url=url,headers=headers,verify=False)
 body = resp.html.find("div#listing-switcher",first=True)
 elements = body.find("div.s-item")
 
+items=[]
 #Data
-job_link=list(elements[0].absolute_links)[0]
-job_title=elements[0].find("h2",first=True).text.strip()
-job_gov=elements[0].find("div.card-text",first=True).text.strip()
-notification_type=elements[0].find("div.card-type",first=True).text.strip()
-candidature_type=elements[0].find("div.btn-sm",first=True).text.strip()
-nbr_place=elements[0].find("div.card-footer",first=True).find('div')[1].text.strip()
-last_date_ofcandidature=elements[0].find("div.card-footer",first=True).find('div')[2].text.strip()
-exam_day=elements[0].find("div.card-footer",first=True).find('div')[3].text.strip()
 
+for ele in elements :
+    job_link=list(ele.absolute_links)[0]
+    job_title=ele.find("h2",first=True).text.strip()
+    job_gov=ele.find("div.card-text",first=True).text.strip()
+    notification_type=ele.find("div.card-type",first=True).text.strip()
+    annonce_or_result=ele.find("div.bg-blue",first=True).text.strip()
+    if ele.find("div.btn-sm",first=True):
+        candidature_methode=ele.find("div.btn-sm",first=True).text.strip()
+    nbr_place=ele.find("div.card-footer",first=True).find('div')[1].text.strip()
+    last_date_ofcandidature=ele.find("div.card-footer",first=True).find('div')[2].text.strip()
+    exam_day=ele.find("div.card-footer",first=True).find('div')[3].text.strip()
+    items.append({"job_title":job_title,"job_gov":job_gov,"job_link":job_link,"candidature_methode":candidature_methode})
+    print(candidature_methode)
 
-
-print(elements[0].find("div.card-footer",first=True).find('div')[3].text.strip())
+#show result    
+for it in items:
+    print()
+    print(it)
+    print()
+    
